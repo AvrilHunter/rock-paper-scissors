f77ed6045bf3fff31eedcbe63c6432f5aec7be82
Avril Hunter <a.hunter@corecomtechacademy.co.uk>
2024-08-16 11:50:22 +0100

    FEAT: game logic added

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,25 +1,31 @@
 public class Game {
 
-
-    public boolean isDraw(String userGuess, String compGuess){
+    private boolean isDraw(String userGuess, String compGuess){
         return userGuess.equals(compGuess);
     }
 
-    public boolean isUserWinner(String userGuess, String compGuess) {
-        if (userGuess.equals("rock") && compGuess.equals("paper")) {
-            return true;
-        } else if (userGuess.equals("paper") && compGuess.equals("rock")) {
-            return false;
-        } else if (userGuess.equals("rock") && compGuess.equals("scissors")) {
-            return true;
-        } else if (userGuess.equals("scissors") && compGuess.equals("rock")) {
-            return false;
-        } else if (userGuess.equals("scissors") && compGuess.equals("paper")) {
-            return true;
-        } else {
-            return true; // if (userGuess.equals("paper")&& compGuess.equals("scissors"))
-        }
+    private boolean isUserWinner(String userGuess, String compGuess) {
+      if(userGuess.equals("rock") && compGuess.equals("scissors")){
+          return true;
+      }else if(userGuess.equals("rock")&& compGuess.equals("paper")){
+          return false;
+      } else if (userGuess.equals("scissors") &&compGuess.equals("rock")) {
+          return false;
+      } else if (userGuess.equals("scissors") && compGuess.equals("paper")) {
+          return true;
+      } else if (userGuess.equals("paper") && compGuess.equals("scissors")) {
+          return false;
+      }else return true; //this covers last option (userGuess.equals("paper") && compGuess.equals("rock")
     }
 
+    public String outcome(String userGuess, String compGuess){
+        if(isDraw(userGuess, compGuess)){
+            return "draw";
+        } else if (isUserWinner(userGuess, compGuess)) {
+            return "User is the winner";
+        }else{
+            return "Computer is the winner";
+        }
+    }
 
 }
